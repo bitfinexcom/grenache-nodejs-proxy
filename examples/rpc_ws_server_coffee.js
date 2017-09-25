6bff4ce13d92626931e9b6e1efead9524b460fc4
fyang1024 <fyang1024@gmail.com>
2017-09-25 20:21:21 +1000

    refactoring

--- examples/rpc_ws_server_coffee.js
+++ examples/rpc_ws_server_coffee.js
@@ -1,7 +1,3 @@
-// make sure you start 2 grapes
-// grape --dp 20001 --aph 30001 --bn '127.0.0.1:20002'
-// grape --dp 20002 --aph 40001 --bn '127.0.0.1:20001'
-
 'use strict'
 
 const Ws = require('grenache-nodejs-ws')
